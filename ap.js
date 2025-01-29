cc79630461e77aeb7f83b21589046bf8ad17de8f
kikikith <kirachest71@gmail.com>
2025-01-29 15:30:41 +0300

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,19 @@
+const video = document.querySelector('.video-background')
+
+const swiperText = new Swiper('.swiper', {
+    speed: 1600,
+    mousewheel: { },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    }
+})
+swiperText.on('slideChange', function(){
+    gsap.to(video, 1.6, {
+        currentTime: (video.duration / this.slides.length) * this.realIndex
+    })
+})
